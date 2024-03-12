2a1fb96d643648dc355e4cc72215e30370416d1f
laszloekovacs <laszloekovacs@gmail.com>
2024-03-12 11:58:47 +0100

    added tailwind

--- src/ExampleFormAction.jsx
+++ src/ExampleFormAction.jsx
@@ -1,26 +1,69 @@
 import { useState } from 'react'
+import { useFormStatus } from 'react-dom'
+
+const Submit = () => {
+	// needs to be in its own component
+	const status = useFormStatus()
+
+	return (
+		<div className='grid gap-8 py-8'>
+			<div>
+				<button
+					type='submit'
+					disabled={status.pending}
+					className='border-1 border border-black px-2 py-1 min-w-[12ch] inline-block font-semibold'>
+					{status.pending ? 'submitting' : 'send'}
+				</button>
+			</div>
+
+			<small>
+				<p>useFormStatus return value</p>
+				<pre>{JSON.stringify(status, null, 2)}</pre>
+			</small>
+		</div>
+	)
+}
 
 export const ExampleFormAction = () => {
 	const [posts, setPosts] = useState([])
 
-	const postMessage = formData => {
+	const postMessage = async formData => {
+		// regular formdata
 		const name = formData.get('name')
 		const message = formData.get('message')
+
+		// delay
+		await new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve()
+			}, 2000)
+		})
 
 		setPosts(posts => [...posts, { name, message }])
 	}
 
 	return (
-		<div>
+		<div className='max-w-[30ch]'>
+			<h1 className='text-2xl my-4'>Form Aciton, Status</h1>
 			<form action={postMessage}>
-				<label>Username</label>
-				<input name='name' title='user' />
-				<br />
+				<div className='grid mb-4'>
+					<label>Username</label>
+					<input
+						name='name'
+						title='user'
+						className='border border-1 p-1 border-gray-700 mt-2'
+					/>
+				</div>
+				<div className='grid'>
+					<label>Message</label>
+					<textarea
+						name='message'
+						title='message'
+						className='border border-1 p-1 border-gray-700 mt-2'
+					/>
+				</div>
 
-				<label>Message</label>
-				<textarea name='message' title='message' />
-
-				<input type='submit' />
+				<Submit />
 			</form>
 
 			<ul>
